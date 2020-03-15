e6e1528cd3110144897aedc097d3cadf99c69c7a
Blaise_Mahoro <blaise.mahoro@eagles.oc.edu>
2020-03-14 22:43:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+name+"!!" );
+            foreach(int i in arr)
+            {
+                if(i%2==0)
+                    Console.WriteLine("Even num: "+i);
+            }
+            
+        }
+    }
+   
+}
